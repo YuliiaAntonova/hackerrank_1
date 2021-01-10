146c0570d3f484b5d9d4391e2151bcbf7c312f12
Yuliia Antonova <yuliia.antonova1@gmail.com>
2021-01-10 20:49:13 +0200

    divisible pairs sum

---
+++ algorithms/divisible pairs sum.py
@@ -0,0 +1,15 @@
+def divisibleSumPairs(arr):
+    count = 0  # Initialize result
+    n=len(arr)
+    k=3
+    # Consider all possible pairs
+    # and check their sums
+    for i in range(0, n):
+        for j in range(i + 1, n):
+            if (arr[i] + arr[j]) % k == 0:
+                count += 1
+
+    return count
+
+
+print(divisibleSumPairs([1, 2, 3, 4, 5, 6]))
